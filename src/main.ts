f76658790ea82edc4bcb92819b5776ed911ea3b4
jesus <pereirajesus35@gmail.com>
2025-10-07 22:01:54 -0400

    enable all cors

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document); // La ruta donde se verá la documentación
    app.enableCors({
     origin:[
+      '*',
       'http://localhost:5173',
       'https://bedega-pos.vercel.app/'] ,// o '*' para cualquier origen
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
