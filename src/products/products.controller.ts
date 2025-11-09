261f92f6d4496dc420965f05e956bbbad2a22c32
jesus <pereirajesus35@gmail.com>
2025-11-09 13:04:42 -0400

    get all products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,17 +4,17 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
-@UseGuards(JwtAuthGuard)
+
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-
+@UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() dto: CreateProductDto) {
     return this.productsService.create(dto);
   }
 
-  @Get()
+  @Get('Public')
   findAll() {
     return this.productsService.findAll();
   }
@@ -23,12 +23,12 @@ export class ProductsController {
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
-
+@UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
     return this.productsService.update(+id, dto);
   }
-
+@UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
